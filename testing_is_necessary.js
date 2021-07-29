612ac4069cbaa011fe48ac0c2ad85a3916246c11
NicoleJoseph18 <87292713+NicoleJoseph18@users.noreply.github.com>
2021-07-29 13:19:31 -0400

    Update testing_is_necessary.js

--- testing_is_necessary.js
+++ testing_is_necessary.js
@@ -11,6 +11,11 @@ funAnimals.push({
     type: "dog",
     reason: "four legs worth of energy at the cost of three"
 })
+funAnimals.push({
+    name: "Peter",
+    type: "turtle",
+    reason: "sturdy wobbler"
+})
 
 for (let animal of funAnimals) {
     console.log(`${animal.name} the ${animal.type} is fun.  Reason: ${animal.reason}`)
